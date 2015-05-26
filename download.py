8c0a46df5f32ee0ed588c3ccf9400cf0d2191faf
Christoph Heuel <mail@christoph-heuel.net>
2015-05-26 09:26:07 +0200

    Functional downloader.py
    * The image will be downloaded and converted
    * adding to cache
    * Write meta-data

--- download.py
+++ download.py
@@ -3,23 +3,54 @@
 import requests
 from lxml import html
 import argparse
+import os
+import io
+from PIL import Image
+import json
 
-class xkcdloader:
+cacheDir=os.path.expanduser("~") + "/.xkcd/"
 
-    def __init__(self):
-        self.cacheDir = "~/.xkcd/"
+def load(number=""):
+    response = requests.get('http://xkcd.com/%s' % number)
+    parsed_body = html.fromstring(response.text)
+    meta_data = {}
+    meta_data["description"] = parsed_body.xpath('//*[@id="comic"]/img/@title')[0]
+    meta_data["name"] = parsed_body.xpath('//*[@id="comic"]/img/@alt')[0]
+    meta_data["image_source"] = "http:%s" % (parsed_body.xpath('//*[@id="comic"]/img/@src')[0])
+    meta_data["instance"] = os.path.split(os.path.splitext(meta_data["image_source"])[0])[1]
+    meta_data["directory"] = "%s%s" % (cacheDir, meta_data["instance"])
+    print(meta_data)
+    if os.path.isdir(meta_data["directory"]):
+        print("Already downloaded: %s" % meta_data["instance"])
+        exit(1)
+    os.makedirs(meta_data["directory"])
+    image = _download_image(meta_data)
+    _convert_image(image, meta_data)
+    _save_meta_data(meta_data)
 
-    def load(self, number=""):
-        response = requests.get('http://xkcd.com/%s' % number)
-        parsed_body = html.fromstring(response.text)
-        print(parsed_body.xpath('//*[@id="comic"]/img/@title'))
-        print(parsed_body.xpath('//*[@id="comic"]/img/@alt'))
-        print(parsed_body.xpath('//*[@id="comic"]/img/@src'))
+def _save_meta_data(data):
+    outfile = open("%s%s%s" % (data["directory"], os.sep, "meta.txt"), "w")
+    json.dump(data, outfile)
+    outfile.close()
+
+def _convert_image(image, data):
+    converted_image = image.convert("1", dither=Image.NEAREST)
+    converted_image.save("%s%s%s" % (data["directory"], os.sep, "converted.png"), "PNG")
+
+def _download_image(data):
+    url2retrieve = data["image_source"]
+    r = requests.get(url2retrieve)
+    if not r.status_code == 200:
+        i = 0
+        raise Exception("Could not retrieve image! Status code %d" % r.status_code)
+    raw_image = Image.open(io.BytesIO(r.content))
+    raw_image.save("%s%s%s" % (data["directory"], os.sep, "image.png"), "PNG")
+    return raw_image
 
 if __name__ == "__main__":
+    if not os.path.exists(cacheDir):
+        os.makedirs(cacheDir)
     parser = argparse.ArgumentParser(description='Load image and description of xkcd')
     parser.add_argument('comic_number', metavar="number", type=str, help="Number of comic to load", default="", nargs="*")
     args = parser.parse_args()
-    print(args.comic_number)
-    xkcdl = xkcdloader()
-    xkcdl.load(args.comic_number)
+    load("" if args.comic_number == "" else args.comic_number[0])
