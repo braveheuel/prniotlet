2da7f3675711867f9482ee3d7055056954392824
Christoph Heuel <mail@christoph-heuel.net>
2015-05-28 01:42:36 +0200

    Load pickled version if existing

--- xkcd.py
+++ xkcd.py
@@ -10,6 +10,7 @@ import shutil
 import pickle
 
 cacheDir=os.path.expanduser("~") + "/.xkcd/"
+pickle_filename = "data.pickle"
 
 def load(number="", overwrite=False):
     response = requests.get('http://xkcd.com/%s' % number)
@@ -20,21 +21,27 @@ def load(number="", overwrite=False):
     meta_data["image_source"] = "http:%s" % (parsed_body.xpath('//*[@id="comic"]/img/@src')[0])
     meta_data["instance"] = os.path.split(os.path.splitext(meta_data["image_source"])[0])[1]
     meta_data["directory"] = "%s%s" % (cacheDir, meta_data["instance"])
-    print(meta_data)
+
     if os.path.isdir(meta_data["directory"]) and overwrite:
         print("Already downloaded %s, removing first..." % meta_data["instance"])
         shutil.rmtree(meta_data["directory"])
-    elif os.path.isdir(meta_data["directory"]):
+
+    if os.path.isdir(meta_data["directory"]):
         print("Already downloaded: %s" % meta_data["instance"])
-        exit(1)
-    os.makedirs(meta_data["directory"])
-    image = _download_image(meta_data)
-    _convert_image(image, meta_data)
-    _save_meta_data(meta_data)
+        print("Using pickled variant")
+        meta_data = _load_from_file(meta_data["directory"])
+    else:
+        os.makedirs(meta_data["directory"])
+        image = _download_image(meta_data)
+        _convert_image(image, meta_data)
+        _save_meta_data(meta_data)
+
+    print(meta_data)
+
     return meta_data
 
 def _save_meta_data(data):
-    with open("%s%s%s" % (data["directory"], os.sep, "meta.txt"), "wb") as outfile:
+    with open("%s%s%s" % (data["directory"], os.sep, pickle_filename), "wb") as outfile:
         pickle.dump(data, outfile)
 
 def _convert_image(image, data):
@@ -43,6 +50,7 @@ def _convert_image(image, data):
     data["image"] = converted_image
 
 def _download_image(data):
+    print("Downloading image...")
     url2retrieve = data["image_source"]
     r = requests.get(url2retrieve)
     if not r.status_code == 200:
@@ -51,6 +59,10 @@ def _download_image(data):
     raw_image = Image.open(io.BytesIO(r.content))
     raw_image.save("%s%s%s" % (data["directory"], os.sep, "image.png"), "PNG")
     return raw_image
+
+def _load_from_file(path):
+    with open("%s%s%s" % (path, os.sep, pickle_filename), "rb") as infile:
+        return pickle.load(infile)
 
 if __name__ == "__main__":
     if not os.path.exists(cacheDir):
