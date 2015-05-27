88a924721506f3ddca7bc45c41315e415df0c6f8
Christoph Heuel <mail@christoph-heuel.net>
2015-05-28 01:17:17 +0200

    Add Parameter to overwrite the files

--- xkcd.py
+++ xkcd.py
@@ -6,11 +6,12 @@ import argparse
 import os
 import io
 from PIL import Image
+import shutil
 import pickle
 
 cacheDir=os.path.expanduser("~") + "/.xkcd/"
 
-def load(number=""):
+def load(number="", overwrite=False):
     response = requests.get('http://xkcd.com/%s' % number)
     parsed_body = html.fromstring(response.text)
     meta_data = {}
@@ -20,7 +21,10 @@ def load(number=""):
     meta_data["instance"] = os.path.split(os.path.splitext(meta_data["image_source"])[0])[1]
     meta_data["directory"] = "%s%s" % (cacheDir, meta_data["instance"])
     print(meta_data)
-    if os.path.isdir(meta_data["directory"]):
+    if os.path.isdir(meta_data["directory"]) and overwrite:
+        print("Already downloaded %s, removing first..." % meta_data["instance"])
+        shutil.rmtree(meta_data["directory"])
+    elif os.path.isdir(meta_data["directory"]):
         print("Already downloaded: %s" % meta_data["instance"])
         exit(1)
     os.makedirs(meta_data["directory"])
@@ -51,5 +55,6 @@ if __name__ == "__main__":
         os.makedirs(cacheDir)
     parser = argparse.ArgumentParser(description='Load image and description of xkcd')
     parser.add_argument('comic_number', metavar="number", type=str, help="Number of comic to load", default="", nargs="*")
+    parser.add_argument('-f', '--overwrite', action='store_true', help="Overwrite if already exists")
     args = parser.parse_args()
     load("" if args.comic_number == "" else args.comic_number[0])
