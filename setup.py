2efaa154e8c01fef7add7bc91510a9b4baf1d3e6
Christoph Heuel <mail@christoph-heuel.net>
2017-07-15 01:36:52 +0200

    Add Setup File

---
+++ setup.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name="prniolet",
+    version="0.0.1",
+    author="Christoph Heuel",
+    author_email="christoph@heuel-web.de",
+    license="MIT",
+    packages=['prniolet'],
+)
