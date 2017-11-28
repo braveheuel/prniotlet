01820f107d4d943772f778330d1e17209e8e3980
Christoph Heuel <mail@christoph-heuel.net>
2017-11-28 23:14:15 +0100

    Update Remote Pi

--- extra/get_and_parse.sh
+++ extra/get_and_parse.sh
@@ -6,6 +6,22 @@
 # Distributed under terms of the MIT license.
 #
 
+DOKPATH="$HOME/Dokumente/advent/2017"
+
+if [ -z "$1" ]
+then
+        START=1
+else
+        START=$1
+fi
+
 mkdir -p out/
 wget --http-user=ch --ask-password https://www.heuel-web.de/owncloud/remote.php/dav/calendars/ch/adventskalender?export -O calendar.ics
-./parseICS.py --start $1 calendar.ics out/
+if [ -f calendar.ics ]
+then
+        ./parseICS.py --start $START calendar.ics "$DOKPATH"
+        rsync -avze ssh $DOKPATH/ raspberrypi:$DOKPATH/
+else
+        echo "Error Retrieving Calendar!"
+        exit 1
+fi
