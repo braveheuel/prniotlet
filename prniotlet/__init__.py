1cc3988070c5531f685b2b981b9c4262461e0ace
Christoph Heuel <christoph@heuel-web.de>
2020-06-20 01:47:56 +0200

    Cleanup of PrnitoLet

--- prniotlet/__init__.py
+++ prniotlet/__init__.py
@@ -1,22 +1,31 @@
 import aiomas
 import asyncio
 from escpos import printer
+import configparser
+from xdg import BaseDirectory
+import os
 
 class PrnIOTlet:
 
-    def __init__(self):
-        self.dummy = printer.Dummy()
+    def __init__(self, cfg_file=None):
+        self.proxy = printer.Dummy()
+        self.config = configparser.ConfigParser()
+        if cfg_file:
+            self.config.read(cfg_file)
+        else:
+            self.config.read(os.path.join(BaseDirectory.xdg_config_home, "prniotlet", "config"))
 
-    @asyncio.coroutine
-    def final_print(self):
-        rpc_con = yield from aiomas.rpc.open_connection(('raspberrypi', 5555), codec=aiomas.MsgPack)
+    async def final_print(self):
+        rpc_con = await aiomas.rpc.open_connection((self.config["connection"]["host"],
+                                                   self.config.getint("connection", "port", fallback=5555)),
+                                                   codec=aiomas.MsgPack)
         try:
-            session = yield from rpc_con.remote.start_session()
-            yield from rpc_con.remote.raw_data(session, self.dummy.output)
-            yield from rpc_con.remote.final_print(session)
-            self.dummy.clear_output()
+            session = await rpc_con.remote.start_session()
+            await rpc_con.remote.raw_data(session, self.proxy.output)
+            await rpc_con.remote.final_print(session)
+            self.proxy.clear()
         except Exception as e:
             print("Error occurred!", e)
         finally:
-            yield from rpc_con.remote.close_session(session)
-            yield from rpc_con.close()
+            await rpc_con.remote.close_session(session)
+            await rpc_con.close()
