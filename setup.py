45fdc1a8e53aa223540a3af442ed4a145022a90e
Christoph Heuel <christoph@heuel-web.de>
2020-07-02 21:09:10 +0200

    Version 0.2.1
    * Correcting the install of package

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup
 
 setup(
     name="prniotlet",
-    version="0.2.0",
+    version="0.2.1",
     author="Christoph Heuel",
     author_email="christoph@heuel-web.de",
     license="MIT",
     install_requires=['python-escpos', 'aiomas[mp]', 'click'],
+    packages=setuptools.find_packages(),
     scripts=[
         'scripts/prniotlet-server', 'scripts/prniotlet-xkcd',
         'scripts/prniotlet-wlan', 'scripts/prniotlet-advent',
